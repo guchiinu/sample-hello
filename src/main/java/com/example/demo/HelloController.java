4630bac501ff2d1767fdd58affc2ee78252f6b57
guchiinu <43141226+guchiinu@users.noreply.github.com>
2020-03-18 21:43:54 +0900

    add file

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.Form;
+
+@Controller
+public class HelloController {
+    //@RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping("/")
+    public String index(Model model) {
+    	model.addAttribute("message", "Hello Spring boot");
+    	model.addAttribute("form", new Form());
+        
+    	return "index";  //表示HTMLファイルの名前（拡張子不要）を指定
+    }       
+}
