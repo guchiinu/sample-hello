83168a1b16c3d79f736783c44b6dc37d08e5b2fa
guchiinu <43141226+guchiinu@users.noreply.github.com>
2020-03-18 21:28:45 +0900

    new

---
+++ src/main/java/com/example/demo/SampleHello3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleHello3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleHello3Application.class, args);
+	}
+
+}
